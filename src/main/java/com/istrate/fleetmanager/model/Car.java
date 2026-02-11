809bd41eb6b6bc3f91a33d6486f54b13219440cb
Istrate Gheorghe-Iustin <justinistrate95@icloud.com>
2026-02-11 18:08:54 +0200

    Initial setup: Spring Boot structure

---
+++ src/main/java/com/istrate/fleetmanager/model/Car.java
@@ -0,0 +1,21 @@
+package com.istrate.fleetmanager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+    private String model;
+    @Column(name = "production_year")
+    private int year;
+    private String licensePlate;
+}
